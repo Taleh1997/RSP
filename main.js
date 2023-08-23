5378e51ea74d17192d19cd0a53ba41dccb932428
Taleh1997 <taleh.huseynov.2012@mail.ru>
2023-08-23 20:31:48 +0400

    second commit

--- main.js
+++ main.js
@@ -1,61 +1,92 @@
-// let userName = prompt("What is your name?")
+// const userName = prompt("What is your name?")
 // userH2.innerHTML = userName
 
 let letterArr = ["r", "s", "p"]
 
-// let userPoint = 0
-// let compPoint= 0
+let userPoints = 0
+let compPoints = 0
 
 const randomCompAttack = (arr) => {
     let compRandomNum = Math.floor(Math.random() * 3)
     return letterArr[compRandomNum]
-}
+};
 
 function gameStart(user, computer) {
     if (user === computer) {
-        console.log("Draf!");
-        h3User.classList.remove("text-success")
-        h3Comp.classList.remove("text-success")
-        h3User.classList.remove("text-danger")
-        h3Comp.classList.remove("text-danger")
-        h3User.innerHTML = "Draf"
-        h3Comp.innerHTML = "Draf"
-        userImg.src = `./img/${user}-img.png`
-        compImg.src = `./img/${computer}-img.png`
+
+        userPoint.innerHTML = `Point: ${userPoints}`;
+        compPoint.innerHTML = `Point : ${compPoints}`;
+
+        h3User.classList.remove("text-success");
+        h3Comp.classList.remove("text-success");
+        h3User.classList.remove("text-danger");
+        h3Comp.classList.remove("text-danger");
+
+        h3User.innerHTML = "Draf";
+        h3Comp.innerHTML = "Draf";
+
+        userImg.src = `./img/${user}-img.png`;
+        compImg.src = `./img/${computer}-img.png`;
+
     } else if ((user === "r" && computer === "s") ||
         (user === "s" && computer === "p") ||
         (user === "p" && computer === "r")) {
-        console.log("You Win!");
-        h3User.classList.add("text-success", )
-        h3User.classList.remove("text-danger")
-        h3Comp.classList.add("text-danger")
-        h3Comp.classList.remove("text-success")
-        // userPoint+=1
-        h3User.innerHTML = "Win!"
-        h3Comp.innerHTML = "Lose!"
-        userImg.src = `./img/${user}-img.png`
+        userPoints++
+        userPoint.innerHTML = `Point: ${userPoints}`;
+        compPoint.innerHTML = `Point : ${compPoints}`;
+
+        h3User.classList.add("text-success");
+        h3User.classList.remove("text-danger");
+        h3Comp.classList.add("text-danger");
+        h3Comp.classList.remove("text-success");
+
+        h3User.innerHTML = "Win!";
+        h3Comp.innerHTML = "Lose!";
+
+        userImg.src = `./img/${user}-img.png`;
         compImg.src = `./img/${computer}-img.png`
+
     } else {
         console.log("Computer Win!");
-        h3User.classList.add("text-danger")
-        h3User.classList.remove("text-success")
-        h3Comp.classList.add("text-success")
-        h3Comp.classList.remove("text-danger")
-        // compPoint+=1
-        h3User.innerHTML = "Lose!"
-        h3Comp.innerHTML = "Win!"
-        userImg.src = `./img/${user}-img.png`
+        compPoints++
+        userPoint.innerHTML = `Point: ${userPoints}`;
+        compPoint.innerHTML = `Point : ${compPoints}`;
+
+        h3User.classList.add("text-danger");
+        h3User.classList.remove("text-success");
+        h3Comp.classList.add("text-success");
+        h3Comp.classList.remove("text-danger");
+
+        h3User.innerHTML = "Lose!";
+        h3Comp.innerHTML = "Win!";
+        
+        userImg.src = `./img/${user}-img.png`;
         compImg.src = `./img/${computer}-img.png`
     }
-}
+};
 
 function play(userChoose) {
     const computer = randomCompAttack(letterArr);
     const result = gameStart(userChoose, computer);
-}
+};
 
+const playGame = (event) => {
+    realPlayerAttack = event.key
+    computer = randomCompAttack(letterArr)
 
+    if (letterArr.indexOf(realPlayerAttack)!==-1) {
+        gameStart(realPlayerAttack, computer)
+    } else {
+        alert("Please, just press one of the 'R S P' button")
+    }
+};
 
 imgRock.addEventListener("click", () => play("r"));
 imgPaper.addEventListener("click", () => play("p"));
-imgScissors.addEventListener("click", () => play("s"))
+imgScissors.addEventListener("click", () => play("s"));
+
+window.onkeydown = playGame
+
+
+
+
